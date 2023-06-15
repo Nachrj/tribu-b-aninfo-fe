a25536400787f4595f75a4b8ef80014c5ec1abaf
Ignacio R.J <38332696+Nachrj@users.noreply.github.com>
2023-06-15 18:18:25 -0300

    Initial changes

--- components/layout.tsx
+++ components/layout.tsx
@@ -6,23 +6,23 @@ import { ISidebarItem } from "./types"
 export default function Layout({ children }: { children: any }) {
   const menuItems: ISidebarItem[] = [
     {
-      href: "/",
-      title: "Homepage",
+      href: "/proyectos",
+      title: "Proyectos",
     },
     {
-      href: "/clientes",
-      title: "Clientes",
+      href: "/soporte",
+      title: "Soporte",
     },
     {
-      href: "/usuarios",
-      title: "Usuarios",
+      href: "/recursos",
+      title: "Recursos",
     }
   ]
 
   return (
     <div className="min-h-screen flex flex-col bg-white">
-      <header className="bg-black sticky top-0 h-14 flex justify-center items-center font-semibold uppercase text-white">
-        Ejemplo pantalla
+      <header style={{ backgroundColor: "purple", fontSize: "1.4em"}} className="sticky h-14 flex justify-center items-center font-semibold  text-white">
+        PSA
       </header>
       <div className="flex flex-col md:flex-row flex-1">
         <aside className="bg-grey-100 w-full md:w-60">
---
+++ pages/soporte/index.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image"
+import { Inter } from "next/font/google"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export default function Support() {
+  return (
+    <div className="flex h-full flex-col justify-center items-center bg-white">
+      <h1 className="text-4xl mb-5 font-bold">soporte</h1>
+    </div>
+  )
+}
