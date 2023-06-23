d139e568e005a6275d95b47f122b4b569804b6fa
SantiagoLocatelli <slocatelli@fi.uba.ar>
2023-06-23 09:41:30 -0300

    adding value into components

--- components/descriptionInput.tsx
+++ components/descriptionInput.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const DescriptionInput = ({ label }) => {
+const DescriptionInput = ({ label, value }) => {
   return (
     <div className="mb-4">
       <label className="block text-gray-800 font-bold mb-4">{label}</label>
       <textarea
+        value={value}
         className="resize-none   overflow-hidden w-full block py-4 border rounded-md focus:outline-none focus:ring focus:border-blue-800 text-black"
       />
       
--- components/input.tsx
+++ components/input.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const Input = ({ label }) => {
+const Input = ({ label, value }) => {
   return (
     <div className="mb-4">
       <label className="block text-gray-800 font-bold mb-4">{label}</label>
       <input
+        value={value}
         className="w-min px-3 py-2 border  rounded-md focus:outline-none focus:ring focus:border-blue-800 text-black"
       />
     </div>
