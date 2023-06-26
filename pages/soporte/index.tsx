d3df6a1f6e70ddc32179220a26a0edec9c9ab454
Nazarena Rueda <50343570+Naza26@users.noreply.github.com>
2023-06-26 18:56:46 -0300

    migrate mui table into products page

--- pages/soporte/index.tsx
+++ pages/soporte/index.tsx
@@ -1,9 +1,10 @@
-import {Product} from "@/pages/types";
 import {useEffect, useState} from "react";
-import ProductGridRow from "@/components/productGridRow";
-import React from 'react';
-import HeaderItem from "@/components/headerItem";
+import Table from "@/components/table";
+import { Box, Button, Container } from "@mui/material";
+import COLORS from "@/constants/colors";
+import Typography from '@mui/material/Typography';
 import { getProducts } from "@/requests/products";
+import { Product } from "../types";
 
 export default function Products() {
     const [products, setProducts] = useState<Product[]>([])
@@ -13,32 +14,29 @@ export default function Products() {
     }, []);
 
     return (
-        <div className="container max-w-7xl mx-auto mt-8">
-            <div className="mb-4">
-                <h1 className="text-black text-3xl font-bold decoration-gray-400">Productos</h1>
-            </div>
-            <div className="flex flex-col">
-                <div className="overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8 w-full">
-                    <div className="inline-block min-w-full overflow-hidden align-middle border-b border-gray-200 shadow sm:rounded-lg text-black">
-                        <table className="min-w-full">
-                            <thead>
-                            <tr>
-                                <HeaderItem title="Name" />
-                                <HeaderItem title="Version" />
-                            </tr>
-                            </thead>
-
-                            <tbody>
-                                {
-                                    products.map((product) => ( 
-                                        <ProductGridRow key={product.id} product={product}/>
-                                    ))
-                                }
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+      <Container component="main" sx={{ backgroundColor: 'white', color: 'black', mt: 4 }}>
+        <Box sx={{ mt: 4 }}>
+          <Box sx={{ display: 'inline-flex', alignItems: 'center', justifyContent: 'space-between', width: '100%'}}>
+            <Typography variant="h3" component="h1">
+              Proyectos
+            </Typography>
+            <Button 
+                type="submit"
+                fullWidth
+                style={{backgroundColor: COLORS.button, height: '50px'}}
+                variant="contained"
+                sx={{ mt: 3, mb: 2, width: '10%' }} 
+                href="./soporte/crear/modificar">
+                  Crear
+            </Button>
+          </Box>
+          
+          <Table 
+            headerItems={["id", "name", "version_name"]}
+            rowItems={products}
+            linkTo="/soporte"
+          />
+        </Box>
+      </Container>
+  )
 }
